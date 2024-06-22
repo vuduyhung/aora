7b184bbb107532f3e4cdd377f56e9a2b1350ba40
Hung Vu <vn.vuduyhung@gmail.com>
2024-06-22 18:19:42 +0700

    setup environment

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-3xl font-pblack">Ahihi Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="profile">Go to Profile</Link>
+    </View>
+  );
+}
